d0af0aea8a342242e81309c0d6f1851bd0ae5017
JerryZhouSirui <jerryzho@andrew.cmu.edu>
2025-01-23 14:52:16 -0500

    Update Circle.java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
@@ -1,12 +1,13 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Circle implements Shape {
-    public double radius;
+    private double radius; // Encapsulate the field
 
     public Circle(double radius) {
         this.radius = radius;
     }
 
+    @Override
     public double getArea() {
         return Math.PI * radius * radius;
     }
