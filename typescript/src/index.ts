63ce68737a4ec04255dc06e792f6e0febdb6ef69
JerryZhouSirui <jerryzho@andrew.cmu.edu>
2025-01-23 15:14:27 -0500

    Fix style in index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,17 +1,17 @@
-import { rectangle } from "./shapes/rectangle.js";
-import { circle } from "./shapes/circle.js";
-import { square } from "./shapes/square.js";
-import { newRenderer } from "./renderer.js";
+import { rectangle } from "./shapes/rectangle.js"
+import { circle } from "./shapes/circle.js"
+import { square } from "./shapes/square.js"
+import { newRenderer } from "./renderer.js"
 
-const rect = rectangle(2, 3); // Rectangle shape
-const circleShape = circle(5); // Circle shape
-const squareShape = square(4); // Square shape
+const rect = rectangle(2, 3) // Rectangle shape
+const circleShape = circle(5) // Circle shape
+const squareShape = square(4) // Square shape
 
-const rectRenderer = newRenderer(rect);
-rectRenderer.draw();
+const rectRenderer = newRenderer(rect)
+rectRenderer.draw()
 
-const circleRenderer = newRenderer(circleShape);
-circleRenderer.draw();
+const circleRenderer = newRenderer(circleShape)
+circleRenderer.draw()
 
-const squareRenderer = newRenderer(squareShape);
-squareRenderer.draw();
+const squareRenderer = newRenderer(squareShape)
+squareRenderer.draw()
